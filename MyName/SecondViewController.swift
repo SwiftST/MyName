5715d48749ae39a28f9bb9ac021c6c03517ffb94
SwiftST <pavel.zubrilov@icloud.com>
2023-06-12 10:36:37 +0500

    add new scene and new UIViewController

---
+++ MyName/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyName
+//
+//  Created by Павел Зубрилов on 12.06.2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
