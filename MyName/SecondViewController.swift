fd5bea43d0380eac93f679b068c5db0583fdcf5c
SwiftST <pavel.zubrilov@icloud.com>
2023-06-12 13:19:20 +0500

    add alertController

--- MyName/SecondViewController.swift
+++ MyName/SecondViewController.swift
@@ -8,13 +8,23 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func showAlert() {
+        let alertController = UIAlertController(
+            title: "Welcome",
+            message: "This is my name App",
+            preferredStyle: .alert)
+        // кнопка ОК
+        let buttonOK = UIAlertAction(
+            title: "OK",
+            style: .default)
+        // кнопка Cancel
+        let buttonCancel = UIAlertAction(
+            title: "Cancel",
+            style: .cancel)
+        // добавление обеих кнопок в AlertController
+        alertController.addAction(buttonOK)
+        alertController.addAction(buttonCancel)
+        // вызов метода present отображающего всплывающее окно
+        self.present(alertController, animated: true)
     }
-    */
 }
